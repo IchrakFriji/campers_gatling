bd2914f1ee2bf201e9f84408fe54a25f1a850fbb
IchrakFriji <ichrakouni00@gmail.com>
2024-06-03 16:32:34 +0100

    add command and product stress tests

---
+++ src/test/scala/com/myGatlingTest/CommandTests.scala
@@ -0,0 +1,56 @@
+package com.myGatlingTest
+
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+
+class CommandTests extends Simulation {
+
+
+  val httpProtocol = http
+    .baseUrl("http://localhost:8091")
+    .header("Content-Type", "application/json")
+
+  val scn = scenario("Post Command")
+    .exec(
+      http("Create Command")
+        .post("/api/commands")
+        .body(StringBody(
+          """{
+            |  "shippingAddress": "123 Main St",
+            |  "method": "Express",
+            |  "confirmed": true,
+            |  "customerFirstName": "John",
+            |  "customerLastName": "Doe",
+            |  "customerEmail": "john.doe@example.com",
+            |  "phoneNumber": "123-456-7890",
+            |  "productCommands": [
+            |    {
+            |      "product": {
+            |        "id": 1,
+            |        "matricule": "PROD001",
+            |        "name": "Sample Product",
+            |        "description": "This is a sample product.",
+            |        "image": "https://example.com/sample-product-image.jpg",
+            |        "discount": 10,
+            |        "price": 99.99,
+            |        "stock": 100,
+            |        "vendingType": "AVENDRE",
+            |        "active": true,
+            |        "createdAt": "2024-05-17T11:26:26.876222100Z",
+            |        "modifiedAt": "2024-05-17T11:26:26.876222100Z"
+            |      },
+            |      "quantity": 1,
+            |      "priceTotal": 99.99
+            |    }
+            |  ]
+            |}""".stripMargin)).asJson
+        .check(status.is(201))
+    )
+
+
+  setUp(
+    scn.inject(atOnceUsers(1000))
+  ).protocols(httpProtocol)
+}
