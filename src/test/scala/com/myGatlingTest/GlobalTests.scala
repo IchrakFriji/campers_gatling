d98466b6ca19a5f8afef3d36fdf1343c388c2ea2
IchrakFriji <ichrakouni00@gmail.com>
2024-06-03 16:49:38 +0100

    update

--- src/test/scala/com/myGatlingTest/GlobalTests.scala
+++ src/test/scala/com/myGatlingTest/GlobalTests.scala
@@ -3,16 +3,33 @@ package com.myGatlingTest
 
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
+import scala.concurrent.duration._
 
-
-class CommandTests extends Simulation {
-
+class GlobalTests extends Simulation {
 
   val httpProtocol = http
     .baseUrl("http://localhost:8091")
     .header("Content-Type", "application/json")
 
-  val scn = scenario("Post Command")
+
+  val scn = scenario("Post Product")
+    .exec(
+      http("Create Product")
+        .post("/api/products")
+        .body(StringBody(
+          """{
+            |"matricule":"PROD001",
+            |"name":"Sample Product",
+            |"description":"This is a sample product.",
+            |"image":"https://example.com/sample-product-image.jpg",
+            |"discount":10,
+            |"price":99.99,
+            |"stock":100,
+            |"vendingType":"AVENDRE",
+            |"active":true
+            |}""".stripMargin)).asJson
+        .check(status.is(200))
+    ).pause(1)
     .exec(
       http("Create Command")
         .post("/api/commands")
@@ -48,6 +65,7 @@ class CommandTests extends Simulation {
             |}""".stripMargin)).asJson
         .check(status.is(201))
     )
+
 
 
   setUp(
