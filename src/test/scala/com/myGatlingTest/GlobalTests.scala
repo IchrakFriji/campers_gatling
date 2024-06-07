8f4e9aadd060bde7255303a4fefe1f24e4c18009
IchrakFriji <ichrakouni00@gmail.com>
2024-06-07 12:12:03 +0100

    update

--- src/test/scala/com/myGatlingTest/GlobalTests.scala
+++ src/test/scala/com/myGatlingTest/GlobalTests.scala
@@ -1,6 +1,5 @@
 package com.myGatlingTest
 
-
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 import scala.concurrent.duration._
@@ -8,67 +7,80 @@ import scala.concurrent.duration._
 class GlobalTests extends Simulation {
 
   val httpProtocol = http
-    .baseUrl("http://spring-app:8091")
+    .baseUrl("http://localhost:8091")
     .header("Content-Type", "application/json")
 
+  var counter = 1
 
-  val scn = scenario("Post Product")
+  val scn = scenario("Global Test")
+    .exec(session => {
+
+      val matricule = java.util.UUID.randomUUID().toString.take(8)
+      session.set("matricule", matricule)
+
+
+    })
+    .exec(session => {
+      counter += 1
+      session.set("id", counter.toString);
+
+    })
     .exec(
       http("Create Product")
-        .post("/api/products")
+        .post("/products")
         .body(StringBody(
-          """{
-            |"matricule":"PROD001",
-            |"name":"Sample Product",
-            |"description":"This is a sample product.",
-            |"image":"https://example.com/sample-product-image.jpg",
-            |"discount":10,
-            |"price":99.99,
-            |"stock":100,
-            |"vendingType":"AVENDRE",
-            |"active":true
-            |}""".stripMargin)).asJson
+          session =>
+            s"""{
+               |"matricule":"${session("matricule").as[String]}",
+               |"name":"Sample Product",
+               |"description":"This is a sample product.",
+               |"image":"https://example.com/sample-product-image.jpg",
+               |"discount":10,
+               |"price":99.99,
+               |"stock":100,
+               |"vendingType":"AVENDRE",
+               |"active":true
+               |}""".stripMargin)).asJson
         .check(status.is(200))
     ).pause(1)
     .exec(
       http("Create Command")
-        .post("/api/commands")
+        .post("/commands")
         .body(StringBody(
-          """{
-            |  "shippingAddress": "123 Main St",
-            |  "method": "Express",
-            |  "confirmed": true,
-            |  "customerFirstName": "John",
-            |  "customerLastName": "Doe",
-            |  "customerEmail": "john.doe@example.com",
-            |  "phoneNumber": "123-456-7890",
-            |  "productCommands": [
-            |    {
-            |      "product": {
-            |        "id": 1,
-            |        "matricule": "PROD001",
-            |        "name": "Sample Product",
-            |        "description": "This is a sample product.",
-            |        "image": "https://example.com/sample-product-image.jpg",
-            |        "discount": 10,
-            |        "price": 99.99,
-            |        "stock": 100,
-            |        "vendingType": "AVENDRE",
-            |        "active": true,
-            |        "createdAt": "2024-05-17T11:26:26.876222100Z",
-            |        "modifiedAt": "2024-05-17T11:26:26.876222100Z"
-            |      },
-            |      "quantity": 1,
-            |      "priceTotal": 99.99
-            |    }
-            |  ]
-            |}""".stripMargin)).asJson
+          session =>
+            s"""{
+               |  "shippingAddress": "123 Main St",
+               |  "method": "Express",
+               |  "confirmed": true,
+               |  "customerFirstName": "John",
+               |  "customerLastName": "Doe",
+               |  "customerEmail": "john.doe@example.com",
+               |  "phoneNumber": "123-456-7890",
+               |  "productCommands": [
+               |    {
+               |      "product": {
+               |        "id": ${session("id").as[String]},
+               |        "matricule": "${session("matricule").as[String]}",
+               |        "name": "Sample Product",
+               |        "description": "This is a sample product.",
+               |        "image": "https://example.com/sample-product-image.jpg",
+               |        "discount": 10,
+               |        "price": 99.99,
+               |        "stock": 100,
+               |        "vendingType": "AVENDRE",
+               |        "active": true,
+               |        "createdAt": "2024-05-17T11:26:26.876222100Z",
+               |        "modifiedAt": "2024-05-17T11:26:26.876222100Z"
+               |      },
+               |      "quantity": 1,
+               |      "priceTotal": 99.99
+               |    }
+               |  ]
+               |}""".stripMargin)).asJson
         .check(status.is(201))
     )
 
-
-
   setUp(
-    scn.inject(atOnceUsers(1000))
+    scn.inject(atOnceUsers(500))
   ).protocols(httpProtocol)
 }
